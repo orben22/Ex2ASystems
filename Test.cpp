c674c43ca03a4ac640135d9a9b56e9fd3b98d43b
or ben ami <orbenami22@gmail.com>
2023-03-28 09:31:25 +0300

    Done

--- Test.cpp
+++ Test.cpp
@@ -16,17 +16,29 @@ TEST_CASE("Before the game started") {
     CHECK_THROWS(Game(p1, p2));
     CHECK_THROWS(game.printLastTurn());
     CHECK_THROWS(game.printWiner());
-    CHECK_THROWS(game.printStats());
     CHECK_THROWS(game.printLog());
+    CHECK_NOTHROW(game.printStats());
     CHECK(((p1.stacksize()==26) && (p2.stacksize()==26)));
+    CHECK(((p1.cardesTaken()==0) && (p2.cardesTaken()==0)));
+    CHECK_NOTHROW(game.playTurn());
 };
 TEST_CASE("During the game"){
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
     game.playTurn();
+    CHECK_THROWS(game.printWiner());
+    CHECK_NOTHROW(game.printLastTurn());
+    CHECK_THROWS(game.printLog());
     CHECK(((p1.cardesTaken()>0) || (p2.cardesTaken()>0)));
     CHECK(((p1.stacksize()<26)&&(p2.stacksize()<26)));
+    int x1=p1.stacksize();
+    int x2=p2.stacksize();
+    game.playTurn();
+    CHECK(((x1>p1.stacksize()) && (x2>p2.stacksize())));
+    CHECK_NOTHROW(game.printStats());
+    CHECK_NOTHROW(game.playTurn());
+    CHECK_NOTHROW(game.playAll());
 };
 
 TEST_CASE("After the game finished"){
@@ -38,6 +50,8 @@ TEST_CASE("After the game finished"){
     CHECK_THROWS(game.playAll());
     CHECK(((p1.stacksize()==0) && (p2.stacksize()==0)));
     CHECK(((p1.cardesTaken()<=52) && (p2.cardesTaken()<=52) && (p1.cardesTaken()>=0) && (p2.cardesTaken()>=0)));
-
-
-}
+    CHECK_NOTHROW(game.printWiner());
+    CHECK_NOTHROW(game.printLastTurn());
+    CHECK_NOTHROW(game.printStats());
+    CHECK_NOTHROW(game.printLog());
+};
